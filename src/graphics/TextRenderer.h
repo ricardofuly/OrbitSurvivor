f8cdf4c4fe04940d5a5a87edb95db2bba4af30fa
Ricardo <lricardofs@gmail.com>
2026-06-19 15:55:53 -0300

    Initial Classes, Window working, ready to continue

---
+++ src/graphics/TextRenderer.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <string>
+#include <unordered_map>
+#include <SDL3_ttf/SDL_ttf.h>
+
+#include "SDL3/SDL_render.h"
+
+struct Vector2;
+
+class TextRenderer
+{
+public:
+    explicit TextRenderer(SDL_Renderer* Renderer);
+    ~TextRenderer();
+    
+    bool Initialize();
+    
+    bool LoadFont(const std::string& ID, const std::string& Path, float Size);
+    void DrawText(const std::string& FontID, std::string& Text, Vector2 Position);
+    
+    void DrawText(const std::string& FontID, const std::string& Text, Vector2 Position, SDL_Color Color);
+    
+private:
+    SDL_Renderer* Renderer = nullptr;
+    
+    std::unordered_map<std::string, TTF_Font*> Fonts;
+};
